1c0890fcd5e031a7e3efa457b85aa9657a2a66f3
hisashihada828-glitch <hisashihada828@gmail.com>
2026-02-15 20:53:41 +0900

    Simplify rendering by removing Router
    Removed Router and useHashLocation from main.tsx.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { Router } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <Router hook={useHashLocation} base="/dragons-players-list-v2">
-    <App />
-  </Router>
-);
+createRoot(document.getElementById("root")!).render(<App />);
