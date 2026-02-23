a1e9b00b8f47618aee3c833d778f30a46c57df77
hisashihada828-glitch <hisashihada828@gmail.com>
2026-02-23 23:47:59 +0900

    阪神追加

--- client/src/lib/playersData.ts
+++ client/src/lib/playersData.ts
@@ -2,10 +2,10 @@ export { TEAMS } from "./teams";
 export type { TeamId } from "./teams";
 export type { Player } from "./playerTypes";
 
-import type { Player } from "./playerTypes";
 import { dragonsPlayers } from "./players/dragons";
 import { baystarsPlayers } from "./players/baystars";
-  
+import { tigersPlayers } from "./players/tigers";
+
 export const playersData: Player[] = [
   ...dragonsPlayers,
   ...baystarsPlayers,
