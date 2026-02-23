b3ac65c9149f329c227981682bcc161676bfbe98
hisashihada828-glitch <hisashihada828@gmail.com>
2026-02-24 00:27:34 +0900

    データ修正（ライオンズ）

--- client/src/lib/players/lions.ts
+++ client/src/lib/players/lions.ts
@@ -1,7 +1,22 @@
-// lions.ts
-import { Player } from "@/lib/types";
+import type { Player } from "../playerTypes";
 
 export const lionsPlayers: Player[] = [
   {
     number: "6",
-    name
+    name: "源田 壮亮",
+    position: "内野手",
+    age: 31,
+    draft: "'16 3位",
+    teamId: "lions",
+    category: "player",
+  },
+  {
+    number: "11",
+    name: "今井 達也",
+    position: "投手",
+    age: 26,
+    draft: "'16 1位",
+    teamId: "lions",
+    category: "player",
+  },
+];
